74765307ec93c4d1f91cd8ea0a9104fe0dd135ab
johannady2 <johannady2@gmail.com>
2015-05-29 17:39:38 +1300

    asdsad

--- www/js/functions.js
+++ www/js/functions.js
@@ -10,11 +10,7 @@ function onBodyLoad()
     document.addEventListener("offline", onDeviceOffline, false);
     document.addEventListener("online", onDeviceOnline, false);
     
-    /*these don't work*/
-     ref.addEventListener('loadstart', function(event) { /*alert('start: ' + event.url);*/ });
-     ref.addEventListener('loadstop', function(event) { /*alert('stop: ' + event.url);*/ });
-     ref.addEventListener('loaderror', function(event) { /*alert('error: ' + event.message);*/ });
-     ref.addEventListener('exit', function(event) { /*alert(event.type);*/});
+
 
     
 }
